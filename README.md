b789f4103fedcfaeefa64515e9c192e930ae4558
LeahighBM <bleahighmc@gmail.com>
2025-06-30 09:24:22 -0600

    small updates to avoid cron. update README

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # nasa_desktop_images
-A script to pull NASA images from the NASA API and set them as your desktop background
+A script to pull NASA images from the NASA API and set them as your desktop background. Now you can have the coolest images this side of the Asteroid Belt as your desktop wallpaper.
+
+![desktop](image/desktop.jpg)
 
 ## Setup 
 Run the `setup.sh` script. This will pull in needed pythons dependencies from the `requirements.txt` file.
 This will also create the necessary directory structure that the python needs to be able to download the images.
 
-Eventually this script will also set up a cron to run the python at a set interval.
+~~Eventually this script will also set up a cron to run the python at a set interval.~~
+
+So the cron was a bit more of hassle than it was worth. After spending more than a few hours trying to get the cron to behave, I pretty much decided that it would be easier to do *something else*. That something else is just a while loop that runs the `main()` function.
+
+### Usage
+Running the script is now a little bit less clean than I originally envisioned. To run, type `python main.py <num_minutes>` to get things up and running. `num_minutes` will be used to configure how long the script will wait before requesting a new wallpaper. If you dont want to have a terminal dedicated to this script try `python main.py <num_minutes> &` to run in the background. Just make sure you take note of the PID so you can kill it later. 
 
 ## API Key
 The use of this script does require the use of an API Key from NASA. Thankfully, it is fairly painless to obtain one of these. To get your own key, navigate to https://api.nasa.gov/ and fill out the form. A key will be sent to the email you provided in the form. Keep this key somewhere you feel is safe. For the purposes of using the scripts in this repo, storing it in an ENV variable called `NASA_API_KEY` is recommended.
 
-If you are just curious about using these scripts and dont want to bother with your own key yet, you may be able to use the NASA `DEMO_KEY` key. Heads up though, this demo key is very limited with only 30 requests per IP per hour.
+If you are just curious about using this scrip and dont want to bother with your own key yet, you may be able to use the NASA `DEMO_KEY` key. Heads up though, this demo key is very limited with only 30 requests per IP per hour.
 
 ## Logging
 Logging is set up to log to the `logs.log` file using pythons built in logging package.
@@ -36,3 +43,7 @@ An example log segment looks like this:
 2025-06-27 10:14:08.605: ERROR | [Errno 2] No such file or directory: 'gsettings set org.cinnamon.desktop.background picture-uri file:///home/brendan/Pictures/Wallpapers/NASA/epic_1b_20250626000830.png'
 
 ```
+You can monitor the log file as it updates in the file itself or with `tail -f path/to/logs` to follow the logs in the terminal.
+
+## Archiving
+At the start of each script run all the existing files (except the current active one) in the wallpaper directory will be moved to the `Archived/` directory. 
--- main.py
+++ main.py
@@ -1,29 +1,18 @@
 import requests
 import os
-import re
 import logging
 import subprocess
 import random
 import sys
 import pathlib
-from enum import Enum
+from time import sleep
 from requests.exceptions import RequestException
 
 
 # TODO: 
-# Figure out what api we want to pull from to start. 
-#   EPIC was good PoC but does not update frequently
-#   Thought: Could do landsat and pull random lat longs from the API 
-#       with each pull. Do every 60-90 mins?
-
-# Write bash script to set up crontab as well as commands for 
-#   setting up directory structure for wallpapers (partially complete)
-
-# ~~Introduce logging~~
-# Re work archive flow to prevent black screen from appearing 
-#   before image is downloaded and set
-
 # retry logic on days where there are no images?
+
+# say screw crons?
 
 NASA_KEY = os.environ.get("NASA_API_KEY")
 BASE_URL = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
@@ -33,7 +22,6 @@ MAX_SOL = 4500
 
 path = pathlib.Path(__file__).resolve().parent
 
-
 logging.basicConfig(
     filename=f"{path}/logs.log",
     level=logging.INFO,
@@ -41,6 +29,24 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 logger = logging.getLogger(__name__)
+
+def archive():
+    logger.info(f"Archving any existing files into {FILE_PATH}/Archived")
+
+    files = [f for f in os.listdir(FILE_PATH) if os.path.isfile(os.path.join(FILE_PATH, f))]
+    files.sort(key=lambda x: os.path.getmtime(os.path.join(FILE_PATH, x)))
+
+    current_wallpaper = files.pop()
+
+    logger.info(f"Archiving files {files}.")
+    logger.info(f"Current background detected as {current_wallpaper}; will be ignored.")
+
+    for file in files:
+        try:
+            os.rename(f"{FILE_PATH}/{file}", f"{FILE_PATH}/Archived/{file}")
+            logger.info(f"File {file} successfully archived.")
+        except FileNotFoundError as e:
+            logger.error("File was not found.", e)
 
 
 def check_headers(header):
@@ -50,22 +56,32 @@ def check_headers(header):
 
 def set_wallpaper(img: str):
     logger.info(f"Attempting to set wallpaper to {img}")
-    command = f"gsettings set org.cinnamon.desktop.background picture-uri file://{FILE_PATH}/{img}"
+    command = f"`gsettings set org.cinnamon.desktop.background picture-uri file://{FILE_PATH}/{img}`"
     logger.info(f"Running command {command}.")
     try:
         subprocess.run(command, shell=True)
     except FileNotFoundError as e:
         logger.error(e)
-        
+
+    logger.info("Wallpaper updated successfully.")   
 
 def main():
 
-    sol = random.randint(1,MAX_SOL)
+    sol = random.randint(1, MAX_SOL)
+
+    if len(sys.argv) != 2:
+        print("Usage: `python main.py <minutes to sleep>`")
+        print("Program exiting with status 1")
+        sys.exit(1)
+    
+    sec_to_sleep = int(sys.argv[1]) * 60
+    logger.info(f"Starting script with sleep time of {sec_to_sleep}s.")
+
     try:
         logger.info(f"Making request out to api.nasa.gov for image information for sol {sol}")
         resp = requests.get(url = f"{BASE_URL}?sol={sol}&api_key={NASA_KEY}")
-        # check_headers(resp.headers)
         resp.raise_for_status()
+        check_headers(resp.headers)
     except RequestException as e:
         logger.error("Error making GET to Baseline URL for image info.", e)
 
@@ -73,20 +89,20 @@ def main():
 
     if len(data["photos"]) == 0:
         logger.warning(f"No images found for Sol {sol}. Program exiting with status 1.")
+        # Without cron this will need definite rework
         sys.exit(1)
     
-    image_url = data["photos"][0]["img_src"]
+    image_url    = data["photos"][0]["img_src"]
     image_sensor = data["photos"][0]["camera"]["name"]
-    image_name = f"SOL_{data["photos"][0]["sol"]}_{image_sensor}_{data["photos"][0]["id"]}"
-    data_type = data["photos"][0]["img_src"][-3:]
+    image_name   = f"SOL_{data["photos"][0]["sol"]}_{image_sensor}_{data["photos"][0]["id"]}"
+    data_type    = data["photos"][0]["img_src"][-3:]
     
     file_name = f"{FILE_PATH}/{image_name}.{data_type}"
     try:
         logger.info(f"Making request to api.nasa.gov for curiosity image from {image_sensor} for SOL {sol}")
         resp = requests.get(image_url, stream=True)
-        # check_headers(resp.headers)
         resp.raise_for_status()
-
+        
         with open(file_name, "wb") as f:
             logger.info(f"Writing to file {file_name}")
             for chunk in resp.iter_content():
@@ -100,18 +116,11 @@ def main():
 
     set_wallpaper(img=f"{image_name}.{data_type}")
 
-def archive():
-    logger.info(f"Archving any existing files into {FILE_PATH}/Archived")
-    files = [f for f in os.listdir(FILE_PATH) if os.path.isfile(os.path.join(FILE_PATH, f))]
-
-    for file in files:
-        try:
-            os.rename(f"{FILE_PATH}/{file}", f"{FILE_PATH}/Archived/{file}")
-            logger.info(f"File {file} successfully archived.")
-        except FileNotFoundError as e:
-            logger.error("File was not found.", e)
+    logger.info(f"Sleeping for {sec_to_sleep}s ({sys.argv[1]} mins) before next request.")
+    sleep(sec_to_sleep)
 
 
 if __name__ == "__main__":
-    # archive()
-    main()
+    archive()
+    while True:
+        main()
